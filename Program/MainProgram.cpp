23ee09d900b6cd6b82736179c51a193c2a08d32b
Jajauma's GitHub <jajauma@2ch.hk>
2018-08-10 11:42:38 +0300

    Enable OpenMP

--- Program/MainProgram.cpp
+++ Program/MainProgram.cpp
@@ -1,5 +1,7 @@
 #include "Options.hpp"
 
+#include <omp.h>
+
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
@@ -11,6 +13,8 @@ mainProgram(int argc, char* argv[])
     try
     {
         Options const programOptions{argc, argv};
+        if (programOptions.getThreadCount() > 0)
+            ::omp_set_num_threads(programOptions.getThreadCount());
         return EXIT_SUCCESS;
     }
     catch (std::exception& e)
